5bec7f621988646682582022fe78cc67706bb9b9
Juan Dorado <jfdoradotr@icloud.com>
2024-11-11 10:30:16 -0600

    Extract the game option buttons

--- RockPaperScissors/RockPaperScissors/ContentView.swift
+++ RockPaperScissors/RockPaperScissors/ContentView.swift
@@ -6,25 +6,45 @@ import SwiftUI
 
 struct ContentView: View {
   var body: some View {
-    HStack {
-      Spacer()
-      Button("🪨") {
-        // Tap on rock
+    HStack(spacing: 60) {
+      ForEach(OptionButton.GameOption.allCases, id: \.self) { option in
+        OptionButton(option: option) {
+
+        }
       }
-      .font(.largeTitle)
-      Spacer()
-      Button("📄") {
-        // Tap on paper
-      }
-      .font(.largeTitle)
-      Spacer()
-      Button("✂️") {
-        // Tap on scissors
-      }
-      .font(.largeTitle)
-      Spacer()
     }
     .padding()
+  }
+}
+
+private extension ContentView {
+  struct OptionButton:View {
+    let option: GameOption
+    let action: () -> Void
+
+    var body: some View {
+      Button(option.text, action: action)
+        .font(.largeTitle)
+    }
+  }
+}
+
+private extension ContentView.OptionButton {
+  enum GameOption: CaseIterable {
+    case rock
+    case paper
+    case scissors
+
+    var text: String {
+      switch self {
+      case .rock:
+        return "🪨"
+      case .paper:
+        return "📄"
+      case .scissors:
+        return "✂️"
+      }
+    }
   }
 }
 
